4daf9cabf111210f71c3d3b50134b2a32ac63327
deadline-cxn <deadline@cityxen.net>
2025-06-28 10:57:46 -0400

    Updates - code prettification

---
+++ README.md
@@ -0,0 +1,13 @@
+# MEATLOAF PROGRAMS
+
+A Collection of Commodore 64 Programs to utilize the Meatloaf / FujiNET device.
+
+- Meatloaf Browser
+A special browser for Commodore 64 using the Meatloaf device
+
+- MLexa
+Control Alexa Smarthome devices with your Meatloaf and Commodore 64
+
+- Youtube Subscribers
+ Shows you how to recieve a YouTube subscriber count for a channel and convert it to Commodore 64 bytes
+ 
--- youtube-subscribers/subs64.php
+++ youtube-subscribers/subs64.php
@@ -5,13 +5,13 @@ $cmd="curl -s \"https://www.googleapis.com/youtube/v3/channels?part=statistics&i
 $cmd.="$channel&key=$apikey\" | jq '.items[0].statistics.subscriberCount'";
 exec($cmd,$x);
 $subs=intval(str_replace("\"","",$x[0]));
-$outsubs_lo=($subs & 65535);// And the low bytes of the subscribers
-$outsubs_hi=($subs >> 16);  // Shift the high bytes to the right 16 times
-echo pack('v', 0xc000);     // C64 program starts at $C000 (49152) (two bytes)
-echo pack('v',$outsubs_lo); // This will pack the subs into the next bytes of the program
-echo pack('v',$outsubs_hi); // Pack high bytes
-                            // the end result: 00,C0,XL,XL,XH,XH
-                            // 6 bytes sent to the C64
+$outsubs_lo=($subs & 65535); // And the low bytes of the subscribers
+$outsubs_hi=($subs >> 16);   // Shift the high bytes to the right 16 times
+echo pack('v', 0xc000);      // C64 program starts at $C000 (49152) (two bytes)
+echo pack('v',$outsubs_lo);  // This will pack the subs into the next bytes of the program
+echo pack('v',$outsubs_hi);  // Pack high bytes
+                             // the end result: 00,C0,XL,XL,XH,XH
+                             // 6 bytes sent to the C64
 
 
                             
