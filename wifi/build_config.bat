0ba7635d3691f30321c9b21349b0fdbcf94e6e5f
deadline-cxn <deadline@cityxen.net>
2026-03-30 20:50:02 -0400

    update

--- wifi/build_config.bat
+++ wifi/build_config.bat
@@ -1,5 +1,6 @@
-petcat -w2 -o c64-wifi-pc.prg config.bas
-exomizer sfx basic -o c64-wifi.prg c64-wifi-pc.prg mlpm.scr.prg
+petcat -w2 -o c64-wifi-preexomizer.prg config.bas
+exomizer sfx basic -o c64-wifi.prg c64-wifi-preexomizer.prg mlpm.scr.prg
+del c64-wifi-preexomizer.prg
 petcat -w2 -o vic20-wifi.prg config-plain.bas
 petcat -w3 -o c16-wifi.prg config-plain.bas
 petcat -w7 -o c128-wifi.prg config-plain.bas
