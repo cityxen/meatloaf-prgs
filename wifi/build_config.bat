8eff236025c95cca605c745fbf4c9513b9ed6ca6
deadline-cxn <deadline@cityxen.net>
2026-03-30 23:55:11 -0400

    fixed multiversion wifi

--- wifi/build_config.bat
+++ wifi/build_config.bat
@@ -1,8 +1,9 @@
 petcat -w2 -o c64-wifi-preexomizer.prg config.bas
-exomizer sfx basic -o wifi-c64.prg c64-wifi-preexomizer.prg mlpm.scr.prg
+exomizer sfx basic -o wifi-c64 c64-wifi-preexomizer.prg mlpm.scr.prg
 del c64-wifi-preexomizer.prg
-petcat -w2 -o wifi-vic20.prg config-plain.bas
-petcat -w3 -o wifi-c16.prg config-plain.bas
-petcat -w7 -o wifi-c128.prg config-plain.bas
+petcat -w2 -o wifi-vic20 config-plain.bas
+petcat -w3 -o wifi-c16 config-plain.bas
+petcat -w7 -o wifi-c128 config-plain.bas
+petcat -w2 -o wifi loader.bas
 
 ftp -s:ftp.u64
