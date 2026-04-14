30e3a57fe586929b3c3f025e788b60589f9116b6
deadline-cxn <deadline@cityxen.net>
2026-04-14 04:29:52 -0400

    update wifi dir structure

--- wifi/build_config.bat
+++ wifi/build_config.bat
@@ -1,9 +1,9 @@
-petcat -w2 -o c64-wifi-preexomizer.prg wifi-c64.bas
-exomizer sfx basic -o include-in-firmware/wifi-c64 c64-wifi-preexomizer.prg wifi-c64.scr.prg
-del c64-wifi-preexomizer.prg
-petcat -w2 -o include-in-firmware/wifi-vic20 wifi-plain.bas
-petcat -w3 -o include-in-firmware/wifi-c16 wifi-plain.bas
-petcat -w70 -o include-in-firmware/wifi-c128 wifi-plain.bas
-petcat -w2 -o include-in-firmware/wifi wifi-loader.bas
-
-
+mkdir prg_files
+petcat -w2 -o src/c64-wifi-preexomizer.prg src/wifi-c64.bas
+exomizer sfx basic -o prg_files/wifi-c64 src/c64-wifi-preexomizer.prg src/meatloaf-c64-screen-sys49152.prg
+del src/c64-wifi-preexomizer.prg
+petcat -w2 -o prg_files/wifi-vic20 src/wifi-plain.bas
+petcat -w3 -o prg_files/wifi-c16 src/wifi-plain.bas
+petcat -w70 -o prg_files/wifi-c128 src/wifi-plain.bas
+petcat -w2 -o prg_files/wifi src/wifi-loader.bas
+7z a prg_files.zip prg_files/*
